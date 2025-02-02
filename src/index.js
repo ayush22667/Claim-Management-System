c537e2ae1988dc8414e132210c95f9342cdfc54e
ayushanand <ayushanand667@gmail.com>
2025-02-02 14:10:35 +0530

    added cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cookieParser = require('cookie-parser');
+const cors = require("cors");
 
 const connectDB = require("./config/database"); // Import MongoDB connection
 require("dotenv").config();
