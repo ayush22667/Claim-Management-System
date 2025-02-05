3315fcb70d68314091449f7961ddee91bb931066
ayushanand <ayushanand667@gmail.com>
2025-02-05 15:49:53 +0530

    backend synced with frontend

---
+++ Backend/src/models/Policy.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const PolicySchema = new mongoose.Schema({
+  policyNumber: { type: String, required: true, unique: true },
+  type: { type: String, required: true },
+  coverageAmount: { type: Number, required: true },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true }
+});
+
+module.exports = mongoose.model("Policy", PolicySchema);
